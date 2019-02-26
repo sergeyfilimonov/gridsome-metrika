7d114511136d1ce1c8aa6c80319a3fe572bd10e4
Sergey <tunecatcher@yandex.ru>
2019-02-26 12:04:10 +0300

    ,

--- gridsome.client.js
+++ gridsome.client.js
@@ -3,7 +3,7 @@ import VueYandexMetrika from 'vue-yandex-metrika'
 export default function (Vue, options, { isServer: disabled, router }) {
   Vue.use(VueYandexMetrika, {
     router: router,
-    env: process.env.NODE_ENV
+    env: process.env.NODE_ENV,
     ...options
   })
 }
